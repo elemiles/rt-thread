601d6a0b00d29883907caa9129f0be1898822c7a
elemiles <maoying@foxmail.com>
2019-12-20 19:27:40 +0800

    1.新增MINI板数据手册及原理图 2.MINI BSP新增EEPROM例子，未完成

--- bsp/stm32/stm32f103-atk-mini/applications/main.c
+++ bsp/stm32/stm32f103-atk-mini/applications/main.c
@@ -12,22 +12,60 @@
 #include <rtdevice.h>
 #include <board.h>
 
+static void eeprom_thread_entry(void *parameter);
+static void eeprom_example(void);
+
 /* defined the LED0 pin: PC0 */
 #define LED0_PIN    GET_PIN(A, 8)
+#define RT_EEPROM_THREAD_STACK_SIZE 1024
+#define RT_EEPROM_THREAD_PRIORITY 9
+
+void eeprom_thread_entry(void *parameter)
+{
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    while (1)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+}
+
+void eeprom_example(void)
+{
+    rt_thread_t tid;
+
+    #ifdef RT_USING_HEAP
+    tid = rt_thread_create("eeprom_thread", eeprom_thread_entry, RT_NULL,
+        RT_EEPROM_THREAD_STACK_SIZE, RT_EEPROM_THREAD_PRIORITY, 0);
+    //tid = RT_NULL;
+    RT_ASSERT(tid != RT_NULL);
+    #else
+    rt_err_t result;
+
+    tid = &main_thread;
+    result = rt_thread_init(tid, "main", main_thread_entry, RT_NULL,
+    main_stack, sizeof(main_stack), RT_MAIN_THREAD_PRIORITY, 20);
+    RT_ASSERT(result == RT_EOK);
+
+    /* if not define RT_USING_HEAP, using to eliminate the warning */
+    (void)result;
+    #endif
+    
+    rt_kprintf("eeprom thread startup!\n\r");
+
+    rt_thread_startup(tid);
+}
+
+
+
 
 int main(void)
 {
-    int count = 1;
-    /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-
-    while (count++)
-    {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(2000);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(2000);
-    }
-
+    //eeprom_example();
     return RT_EOK;
 }
+
+
+MSH_CMD_EXPORT(eeprom_example, eeprom example);
--- bsp/stm32/stm32f429-atk-apollo/rtconfig.h
+++ bsp/stm32/stm32f429-atk-apollo/rtconfig.h
@@ -73,41 +73,103 @@
 
 /* Device virtual file system */
 
+#define RT_USING_DFS
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 2
+#define DFS_FILESYSTEM_TYPES_MAX 2
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
 
 #define RT_USING_DEVICE_IPC
 #define RT_PIPE_BUFSZ 512
+#define RT_USING_SYSTEM_WORKQUEUE
+#define RT_SYSTEM_WORKQUEUE_STACKSIZE 2048
+#define RT_SYSTEM_WORKQUEUE_PRIORITY 23
 #define RT_USING_SERIAL
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_I2C
+#define RT_I2C_DEBUG
+#define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
-
-/* Using Hardware Crypto drivers */
-
-
-/* Using WiFi */
-
+#define RT_USING_SPI
+#define RT_USING_SFUD
+#define RT_SFUD_USING_SFDP
+#define RT_SFUD_USING_FLASH_INFO_TABLE
+#define RT_USING_WDT
 
 /* Using USB */
 
 
 /* POSIX layer and C standard library */
 
+#define RT_USING_LIBC
+#define RT_USING_POSIX
 
 /* Network */
 
 /* Socket abstraction layer */
 
+#define RT_USING_SAL
+
+/* protocol stack implement */
+
+#define SAL_USING_LWIP
+#define SAL_USING_POSIX
 
 /* Network interface device */
 
+#define RT_USING_NETDEV
+#define NETDEV_USING_IFCONFIG
+#define NETDEV_USING_PING
+#define NETDEV_USING_NETSTAT
+#define NETDEV_USING_AUTO_DEFAULT
+#define NETDEV_IPV4 1
+#define NETDEV_IPV6 0
 
 /* light weight TCP/IP stack */
 
+#define RT_USING_LWIP
+#define RT_USING_LWIP202
+#define RT_LWIP_IGMP
+#define RT_LWIP_ICMP
+#define RT_LWIP_DNS
+#define RT_LWIP_DHCP
+#define IP_SOF_BROADCAST 1
+#define IP_SOF_BROADCAST_RECV 1
 
-/* Modbus master and slave stack */
+/* Static IPv4 Address */
 
+#define RT_LWIP_IPADDR "192.168.1.30"
+#define RT_LWIP_GWADDR "192.168.1.1"
+#define RT_LWIP_MSKADDR "255.255.255.0"
+#define RT_LWIP_UDP
+#define RT_LWIP_TCP
+#define RT_LWIP_RAW
+#define RT_MEMP_NUM_NETCONN 8
+#define RT_LWIP_PBUF_NUM 16
+#define RT_LWIP_RAW_PCB_NUM 4
+#define RT_LWIP_UDP_PCB_NUM 4
+#define RT_LWIP_TCP_PCB_NUM 4
+#define RT_LWIP_TCP_SEG_NUM 40
+#define RT_LWIP_TCP_SND_BUF 8196
+#define RT_LWIP_TCP_WND 8196
+#define RT_LWIP_TCPTHREAD_PRIORITY 10
+#define RT_LWIP_TCPTHREAD_MBOX_SIZE 8
+#define RT_LWIP_TCPTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_PRIORITY 12
+#define RT_LWIP_ETHTHREAD_STACKSIZE 1024
+#define RT_LWIP_ETHTHREAD_MBOX_SIZE 8
+#define LWIP_NETIF_STATUS_CALLBACK 1
+#define LWIP_NETIF_LINK_CALLBACK 1
+#define SO_REUSE 1
+#define LWIP_SO_RCVTIMEO 1
+#define LWIP_SO_SNDTIMEO 1
+#define LWIP_SO_RCVBUF 1
+#define LWIP_NETIF_LOOPBACK 0
+#define RT_LWIP_USING_PING
 
 /* AT commands */
 
@@ -167,12 +229,21 @@
 /* Onboard Peripheral Drivers */
 
 #define BSP_USING_USB_TO_USART
+#define BSP_USING_SPI_FLASH
 
 /* On-chip Peripheral Drivers */
 
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
+#define BSP_USING_SPI
+#define BSP_USING_SPI5
+#define BSP_USING_I2C1
+
+/* Notice: PH4 --> 116; PH5 --> 117 */
+
+#define BSP_I2C1_SCL_PIN 116
+#define BSP_I2C1_SDA_PIN 117
 
 /* Board extended module Drivers */
 
